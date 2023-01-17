20e60de0834aec50c0584b481e53ba062786385d
Steffina1 <105663830+Steffina1@users.noreply.github.com>
2023-01-17 17:20:24 -0600

    Update UpdateBatchById.java

--- src/java/stepdefinitions/UpdateBatchById.java
+++ src/java/stepdefinitions/UpdateBatchById.java
@@ -1,5 +1,226 @@
 package com.lmshackathon.stepdefiniton;
 
 public class UpdateBatchById {
+  import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+public class UpdateBatchById {
+	@Given("User sends request with its endpoint to update by Batch Id")
+	public void user_sends_request_with_its_endpoint_to_update_by_batch_id() {
+	    
+	}
+
+	@Given("Verify response body validation.")
+	public void verify_response_body_validation() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("User sends request body with a valid Json Schema to update Batch Id")
+	public void user_sends_request_body_with_a_valid_json_schema_to_update_batch_id() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("Check creation time and last modification time should be the current time.")
+	public void check_creation_time_and_last_modification_time_should_be_the_current_time() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("Validate Put Schema in json format.")
+	public void validate_put_schema_in_json_format() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("User should receive response with status code {string}")
+	public void user_should_receive_response_with_status_code(String string) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("Check the DB validation")
+	public void check_the_db_validation() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Given("User sends request with its invalid Batch Id")
+	public void user_sends_request_with_its_invalid_batch_id() {
+		response=httprequest.delete();
+		System.out.println(response.asPrettyString());
+	}
+
+	@Given("Verify response headers should be application\\/json format.")
+	public void verify_response_headers_should_be_application_json_format() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("User sends request body with invalid base url and its endpoint")
+	public void user_sends_request_body_with_invalid_base_url_and_its_endpoint() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("Verify response with status code {string} .")
+	public void verify_response_with_status_code(String string) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Given("User sends a invalid request to update Batch Id")
+	public void user_sends_a_invalid_request_to_update_batch_id() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Given("Verify response body validation with status {string} and error message {string}")
+	public void verify_response_body_validation_with_status_and_error_message(String string, String string2) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("User sends request body with its endpoints.")
+	public void user_sends_request_body_with_its_endpoints() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("Validate Post Schema in json format.")
+	public void validate_post_schema_in_json_format() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("Verify response with status code {string}")
+	public void verify_response_with_status_code(String string) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Given("User sends request with invalid endpoint")
+	public void user_sends_request_with_invalid_endpoint() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Given("Validate response body with error message {string}.")
+	public void validate_response_body_with_error_message(String string) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("User sends request body with valid data.")
+	public void user_sends_request_body_with_valid_data() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Given("User sends request with its endpoint to update Batch Id")
+	public void user_sends_request_with_its_endpoint_to_update_batch_id() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("User sends request body with invalid Json Schema.")
+	public void user_sends_request_body_with_invalid_json_schema() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Given("User sends request to update multiple Program through external file.")
+	public void user_sends_request_to_update_multiple_program_through_external_file() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("User sends request body to update multiple Programs through external file.")
+	public void user_sends_request_body_to_update_multiple_programs_through_external_file() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Given("User sends request with already existing data to Batch Id")
+	public void user_sends_request_with_already_existing_data_to_batch_id() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Given("Verify response body validation")
+	public void verify_response_body_validation() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("User sends the request body with existing Batch Id")
+	public void user_sends_the_request_body_with_existing_batch_id() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Given("User sends request with non existing data to Batch Id")
+	public void user_sends_request_with_non_existing_data_to_batch_id() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Given("Verify response body validation with error code.")
+	public void verify_response_body_validation_with_error_code() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("User sends the request body with non existing Batch Id    And Check creation time and last modification time should be the current time.")
+	public void user_sends_the_request_body_with_non_existing_batch_id_and_check_creation_time_and_last_modification_time_should_be_the_current_time() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Given("User sends empty input fields to Batch Id")
+	public void user_sends_empty_input_fields_to_batch_id() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("User sends request body with empty input data to update Batch Id    And Check creation time and last modification time should be the current time.")
+	public void user_sends_request_body_with_empty_input_data_to_update_batch_id_and_check_creation_time_and_last_modification_time_should_be_the_current_time() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Given("I want to write a step with name1")
+	public void i_want_to_write_a_step_with_name1() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("I check for the {int} in step")
+	public void i_check_for_the_in_step(Integer int1) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("I verify the success in step")
+	public void i_verify_the_success_in_step() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Given("I want to write a step with name2")
+	public void i_want_to_write_a_step_with_name2() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("I verify the Fail in step")
+	public void i_verify_the_fail_in_step() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+}
+
+
 
 }
